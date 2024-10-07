9fdb8bf3a985c872335006f41b75a73d5567e363
Oier Amelibia <oamelibia001@ikasle.ehu.eus>
2024-10-07 22:52:32 +0200

    js funtzioak egin

--- js/bookviewer.js
+++ js/bookviewer.js
@@ -21,37 +21,72 @@ export class BookViewer {
         // aurrera, atzera eta bilatu botoiak hasieratu
         // bilatu botoia sakatzean, erabiltzaileak sartu duen isbn-a duen liburua lortu
         // eta handleSearchData funtzioa exekutatu
-        
+        document.getElementById('aurrera').addEventListener('click', () => this.nextBook());
+        document.getElementById('atzera').addEventListener('click', () => this.prevBook());
+        document.getElementById('bilatu').addEventListener('click', () => {
+            const isbn = this.isbn.value;
+            this.searchBookByISBN(isbn);
+        });
     }
 
     extractBookData = (book) => {
         // json objektu egoki bat bueltatu, zure webgunean erabili ahal izateko
-        return null;
-      };
-      
+        const { cover_i, title, author_name, first_publish_year, isbn } = book;
+        return {
+            filename: cover_i ? `${cover_i}-M.jpg` : "default-cover.jpg",
+            izenburua: title || "Unknown Title",
+            egilea: author_name && author_name.length > 0 ? author_name[0] : "Unknown Author",
+            data: first_publish_year || "Unknown Year",
+            isbn: isbn && isbn.length > 0 ? isbn[0] : "Unknown ISBN"
+        };
+    }
     addBookToData = (book, data) => {
         // data array-ean sartu liburua, eta liburu berriaren posizioa bueltatu
-        return null;
+        data.push(book);
+        return data.length - 1;
     };
 
     handleSearchData = (data) => {
         // lortu liburua data objektutik
         // extractBookData eta addBookToData funtzioak erabili, indizea berria lortuz
         // updateView funtzioa erabili, liburu berria bistaratzeko
+        const book = data.docs?.[0];
+        if (book) {
+            const bookData = this.extractBookData(book);
+            this.index = this.addBookToData(bookData, this.data);
+            this.updateView();
+        } else {
+            alert("Ez da liburua aurkitu");
+        }
     };
 
     updateView() {
         // liburuaren datu guztiak bistaratu
         // liburu kopurua bistaratu
+        const { filename, izenburua, egilea, data, isbn } = this.data[this.index];
+        this.irudia.src = `${this.base}${filename}`;
+        this.izenburua.value = izenburua;
+        this.egilea.value = egilea;
+        this.dataElem.value = data;
+        this.isbn.value = isbn;
+        this.liburuKopuru.innerText = `${this.index + 1}/${this.data.length}`;
     }
 
     nextBook() {
         // Hurrengo indizea lortu eta updateView funtzioa erabili bistaratzeko
         // ez ezazu liburu kopurua gainditu
+        if (this.index < this.data.length - 1) {
+            this.index++;
+            this.updateView();
+        }
     }
 
     prevBook() {
         // Aurreko indizea lortu eta updateView funtzioa erabili bistaratzeko
         // ez ezazu 0 indizea gainditu
+        if (this.index > 0) {
+            this.index--;
+            this.updateView();
+        }
     }
 }
